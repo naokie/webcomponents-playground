c45a16965cbca778a68ab3fe9bb5c796386e75a5
Naoki Endoh <naoki.endoh@gmail.com>
2017-08-03 15:19:37 +0900

    Add shadow root

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 class Counter extends HTMLElement {
   constructor() {
     super();
+    this.shadow = this.createShadowRoot();
   }
 
   connectedCallback() {
     console.log('connected');
+
+    const template = `
+      <div>123</div>
+    `;
+
+    this.shadow.innerHTML = template;
   }
 }
 
